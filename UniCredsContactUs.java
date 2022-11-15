eb2efd78239c7f0b3d8eaf8d88aee877c0b2a0f1
Atul Kumar Mishra <107622509+Atulslife@users.noreply.github.com>
2022-11-15 14:19:50 +0530

    Update UniCredsContactUs.java

--- UniCredsContactUs.java
+++ UniCredsContactUs.java
@@ -7,7 +7,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -15,18 +17,27 @@ public class UniCredsContactUs {
 	WebDriver driver;
 
 	@BeforeTest
-	public void beforeTest() {
+	public void OpenBrowser() {
 		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 	}
 
-	@Test(priority = 1)
-	public void contactUsFrom() {
+	@BeforeMethod
+	public void OpenUrl() {
 		driver.get("https://unicreds.com/contact-us");
+	}
+
+	@Test(priority = 1)
+	public void WaitforAlert() {
 		new WebDriverWait(driver, Duration.ofSeconds(20)).until(ExpectedConditions.presenceOfAllElementsLocatedBy(
 				By.xpath("//html/body/div[4]/div/div/div[2]/div/div/div/div/div[2]/div")));
 		driver.findElement(By.xpath("//html/body/div[4]/div/div/div[1]/button/span[1]")).click();
+		
+	}
+
+	@Test(priority = 2)
+	public void contactUsFrom() {
 		driver.findElement(By.id("fullname")).sendKeys("atul");
 		driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("mishraatul65@gmail.com");
 		Select s1 = new Select(driver
@@ -37,8 +48,13 @@ public class UniCredsContactUs {
 		driver.findElement(By.id("contactButton")).click();
 	}
 
+	@AfterMethod
+	public void Submit() {
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+	}
+
 	@AfterTest
-	public void afterTest() {
+	public void CloseBrowserTab() {
 		driver.close();
 	}
 }
